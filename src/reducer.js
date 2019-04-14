3be76ecb215e9e454c7009f0d2bcf4a0057626e9
Matthew <vickery.matthew@gmail.com>
2019-04-14 23:54:35 +0200

    Add missing firestore reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as firebase } from 'react-redux-firebase';
+import { reducer as firestore } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   firebase,
+  firestore,
 });
 
 export default rootReducer;
